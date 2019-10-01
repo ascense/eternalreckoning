7c6bcd2a42402f6bb2e24a94b7d3a72be34f0783
Henry Carlson <henry.carlson@gmail.com>
2019-10-01 19:35:53 +0300

    Move logging configuration to core

--- core/src/util/logging.rs
+++ core/src/util/logging.rs
@@ -25,7 +25,7 @@ pub enum LogLevel {
     Trace,
 }
 
-pub fn configure(config: &LoggingConfig) -> Result<(), Error> {
+pub fn configure(config: &LoggingConfig, component: &'static str) -> Result<(), Error> {
     let level = match config.level {
         LogLevel::Error => log::LevelFilter::Error,
         LogLevel::Warn => log::LevelFilter::Warn,
@@ -46,7 +46,7 @@ pub fn configure(config: &LoggingConfig) -> Result<(), Error> {
         })
         .level(log::LevelFilter::Warn)
         .level_for("eternalreckoning_core", level)
-        .level_for("eternalreckoning_server", level)
+        .level_for(component, level)
         .chain(std::io::stdout());
     
     if let Some(ref path) = config.file {
